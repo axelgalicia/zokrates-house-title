ca8cf21e4898cd8d872c5d6db06d431595cb77cc
Axel Galicia <axelgalicia@gmail.com>
2021-09-29 17:18:24 -0400

    Creating verifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -2,60 +2,29 @@
 // This file is LGPL3 Licensed
 pragma solidity 0.8.1;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+import "./ERC721Mintable.sol";
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721Mintable {
+    // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+    Verifier private verifierContract;
 
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 solutionIndex;
+        address solutionAddress;
+        bool minted; //flag to indicate if this solution has been used in token minting
+    }
 
+    // TODO define an array of the above struct
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO define a mapping to store unique solutions submitted
 
+    // TODO Create an event to emit when a solution is added
 
-// TODO define an array of the above struct
+    // TODO Create a function to add the solutions to the array and emit the event
 
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+}
